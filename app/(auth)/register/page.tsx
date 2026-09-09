6ea6ad5e16b35500e4927d1f3231232bfc25f6c4
hariom-ojha23 <hariomojha2301@gmail.com>
2026-09-09 23:38:57 +0530

    feat: regitser form

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,3 +1,35 @@
+import { Button } from '@/components/ui/button'
+import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from '@/components/ui/card'
+import RegisterForm from '@/features/auth/components/RegisterForm'
+import { cn } from 'cn'
+import Link from 'next/link'
+
 export default function RegisterPage() {
-  return <div>Register</div>
+  const linkClass = 'hover:underline hover:text-blue-400 cursor-pointer'
+
+  return (
+    <Card className="w-full max-w-sm">
+      <CardHeader>
+        <CardTitle>Create new account</CardTitle>
+        <CardDescription>Enter your name, email & password to create your account</CardDescription>
+      </CardHeader>
+
+      <CardContent>
+        <RegisterForm />
+      </CardContent>
+
+      <CardFooter className="flex-col gap-4">
+        <Button type="submit" form="register-form" className="w-full">
+          Register
+        </Button>
+
+        <p>
+          Already have an account?
+          <Link href="/login" className={cn(linkClass, 'ml-1')}>
+            Login
+          </Link>
+        </p>
+      </CardFooter>
+    </Card>
+  )
 }
---
+++ features/auth/components/RegisterForm.tsx
@@ -0,0 +1,104 @@
+'use client'
+
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { RegisterInput, registerSchema } from '../schemas'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { InputGroup, InputGroupAddon, InputGroupInput } from '@/components/ui/input-group'
+import { EyeIcon, EyeOffIcon } from 'lucide-react'
+import { register as registerUser } from '../api'
+
+export default function RegisterForm() {
+  const router = useRouter()
+
+  const [showPassword, setShowPassword] = useState<boolean>(false)
+  const [serverError, setServerError] = useState<string | null>(null)
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting }
+  } = useForm<RegisterInput>({
+    resolver: zodResolver(registerSchema),
+    defaultValues: { name: '', email: '', password: '' }
+  })
+
+  const onSubmit = async (data: RegisterInput) => {
+    try {
+      setServerError(null)
+      await registerUser(data)
+
+      router.push('/dashboard')
+      router.refresh()
+    } catch (error) {
+      const message = error instanceof Error ? error.message : 'Unable to register. Please try again.'
+      setServerError(message)
+    }
+  }
+
+  return (
+    <form id="register-form" onSubmit={handleSubmit(onSubmit)}>
+      <div className="space-y-5">
+        {serverError && <p className="text-xs text-destructive">{serverError}</p>}
+
+        <div className="space-y-2">
+          <Label htmlFor="name">Full Name</Label>
+          <Input
+            id="name"
+            type="name"
+            required
+            placeholder="Enter your name"
+            autoComplete="name"
+            disabled={isSubmitting}
+            {...register('name')}
+          />
+
+          {errors.name && <p className="text-xs text-destructive">{errors.name.message}</p>}
+        </div>
+
+        <div className="space-y-2">
+          <Label htmlFor="email">Email</Label>
+          <Input
+            id="email"
+            type="email"
+            required
+            placeholder="you@example.com"
+            autoComplete="email"
+            disabled={isSubmitting}
+            {...register('email')}
+          />
+
+          {errors.email && <p className="text-xs text-destructive">{errors.email.message}</p>}
+        </div>
+
+        <div className="space-y-2">
+          <Label htmlFor="password">Password</Label>
+          <InputGroup>
+            <InputGroupInput
+              id="password"
+              type={showPassword ? 'text' : 'password'}
+              required
+              minLength={6}
+              placeholder="••••••••"
+              disabled={isSubmitting}
+              {...register('password')}
+            />
+
+            <InputGroupAddon
+              className="cursor-pointer"
+              align="inline-end"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? <EyeOffIcon /> : <EyeIcon />}
+            </InputGroupAddon>
+          </InputGroup>
+
+          {errors.password && <p className="text-xs text-destructive">{errors.password.message}</p>}
+        </div>
+      </div>
+    </form>
+  )
+}
